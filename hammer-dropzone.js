597af52ea2f7460b44323aa85337adfd6a9bbf0c
Erick Zanardo <erickzanardoo@gmail.com>
2014-12-16 16:38:09 -0200

    adding classes for better display purpouse

--- hammer-dropzone.js
+++ hammer-dropzone.js
@@ -2,26 +2,44 @@
     $.fn.hammerDropZone = function(dropZoneSelector, onDrop) {
         
         $(this).each(function() {
+            
+            
+            
             var hammertime = Hammer(this);
             var obj = null;
             var middleX;
             var middleY;
 
+            var dropZones = $(dropZoneSelector);
+            var dropZonesObjs = [];
+
+            
+            var currPos = function(evt) {
+                return {
+                    currX: evt.center.x - middleX,
+                    currY: evt.center.y - middleY
+                };
+            };
+            
+            var verifyDropZoneBounds = function(pos, onHit, onMiss) {
+                for (var i = 0; i < dropZonesObjs.length; i++) {
+                    var dz = dropZonesObjs[i];
+
+                    if (((pos.currX >= dz.x && pos.currX <= dz.x2) && (pos.currY >= dz.y && pos.currY <= dz.y2))) {
+                        if (onHit) {
+                            onHit(dz);
+                        }
+                    } else {
+                        if (onMiss) {
+                            onMiss(dz);
+                        }
+                    }
+                }
+            };
+            
             hammertime.on('panstart', function(evt) {
-                obj = $(evt.target).clone().appendTo('body');
-                obj.css('position', 'absolute');
-                middleX = obj.width() / 2;
-                middleY = obj.height() / 2;
-            });
 
-            hammertime.on('pan', function(evt) {
-                obj.css('left', evt.center.x - middleX);
-                obj.css('top', evt.center.y - middleY);
-            });
-
-            hammertime.on('panend', function(evt) {
-                var dropZones = $(dropZoneSelector);
-                var hit = false;
+                // Calculate the dropzones bounds
                 dropZones.each(function() {
                     var me = $(this);
                     var offset = me.offset();
@@ -32,14 +50,62 @@
                     var y = offset.top;
                     var y2 = y + me.height();
 
-                    var currX = evt.center.x - middleX;
-                    var currY = evt.center.y - middleY;
+                    dropZonesObjs.push({
+                        obj: me,
+                        x: x,
+                        x2: x2,
+                        y: y,
+                        y2: y2
+                    });
+                });
+                
+                var panObject = $(evt.target);
+                var clone;
+                if (panObject.is('.droppable')) {
+                    clone = panObject.clone();
+                } else {
+                    clone = panObject.parents('.droppable').clone();
+                }
+                
+                obj = clone.appendTo('body');
+                obj.css('position', 'absolute');
+                middleX = obj.width() / 2;
+                middleY = obj.height() / 2;
+                
+                $(dropZoneSelector).addClass('drop-zone-target');
+                
+                evt.preventDefault();
+            });
 
-                    if (((currX >= x && currX <= x2) && (currY >= y && currY <= y2))) {
-                        hit = true;
-                        onDrop(obj, me);
+            hammertime.on('pan', function(evt) {
+                obj.css('left', evt.center.x - middleX);
+                obj.css('top', evt.center.y - middleY);
+                
+                var pos = currPos(evt);
+                verifyDropZoneBounds(pos, function(dz) {
+                    if (!dz.obj.is('.drop-zone-target-hover')) {
+                        dz.obj.addClass('drop-zone-target-hover')
+                    }
+                }, function(dz) {
+                    if (dz.obj.is('.drop-zone-target-hover')) {
+                        dz.obj.removeClass('drop-zone-target-hover')
                     }
                 });
+                
+                evt.preventDefault();
+            });
+
+            hammertime.on('panend', function(evt) {
+                dropZones.removeClass('drop-zone-target');
+                dropZones.removeClass('drop-zone-target-hover');
+                var hit = false;
+
+                var pos = currPos(evt);
+                verifyDropZoneBounds(pos, function(dz) {
+                    hit = true;
+                    onDrop(obj, dz.obj);
+                });
+
                 if (!hit) {
                     obj.remove();
                 }
